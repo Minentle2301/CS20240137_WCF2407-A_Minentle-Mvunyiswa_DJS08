9cf6a6b075f83395526dbfec12fd0a6665b91220
minentle mvunyiswa <minentlemvunyiswa2001@gmail.com>
2024-11-15 13:30:40 +0200

    updated the links and useparams and useSearchparams

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import Vans from "./pages/Vans.jsx";
+import VanDetails from "./pages/VanDetails.jsx";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetails />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
